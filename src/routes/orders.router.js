bdaa429133c29d338948e855c010ee81229b518c
MartinPerezDev <martinperez9713@gmail.com>
2026-09-17 00:06:41 -0300

    clase 1: arquitectura por capas

---
+++ src/routes/orders.router.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { getOrders, getOrderById, createOrder, updateOrderStatus, deleteOrder } from "../controller/order.controller.js";
+
+const router = Router();
+
+router.get("/", getOrders);
+
+router.get("/:oid", getOrderById);
+
+router.post("/", createOrder);
+
+router.put("/:oid/status", updateOrderStatus);
+
+router.delete("/:oid", deleteOrder);
+
+export default router;
